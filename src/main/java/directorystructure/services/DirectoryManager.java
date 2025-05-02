af07e52e873d9dc4bf39e701d57460a181ccc134
Sorour <sorour.jahanbin@yahoo.com>
2025-05-02 13:29:38 +0200

    Add new functionalities
    Add filterBy methods and also printing tree
    There is an issue with ordering

---
+++ src/main/java/directorystructure/services/DirectoryManager.java
@@ -0,0 +1,87 @@
+package directorystructure.services;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import directorystructure.domainmodel.DirectoryNode;
+import directorystructure.domainmodel.FileNode;
+import directorystructure.domainmodel.Node;
+
+public class DirectoryManager {
+
+	private static String tree = "";
+	static List<FileNode> filteredNodes = new ArrayList<FileNode>();
+
+	public static String printTree(Node node) {
+
+		DirectoryNode root = (DirectoryNode) node;
+		root.getChildren().sort(Comparator.comparing(Node::getName));// sort is case sensitive
+		tree = root.toString() + "\n";
+		treeIterator(root.getChildren(), 0);
+		return tree;
+	}
+
+	private static void treeIterator(List<Node> children, int indentation) {
+		for (Node child : children) {
+
+			if (child instanceof FileNode) {
+				tree += String.format("%s %s \n", " ".repeat(indentation), child.toString());
+			} else if (child instanceof DirectoryNode) {
+				tree += String.format("%s %s \n", " ".repeat(indentation), child.toString());
+				treeIterator(((DirectoryNode) child).getChildren(), indentation + 1);
+			}
+		}
+	}
+
+	public static String filterTopSecretFiles(Node node) {
+		filteredNodes = new ArrayList<FileNode>();
+		StringBuilder builder = new StringBuilder();
+		for (FileNode child:filterByClassification(node, "Top Secret")) {
+				builder.append(child.toString()+"\n");
+		}
+		return builder.toString().trim();
+	}
+
+	public static String filterSecretFiles(Node node) {
+		filteredNodes = new ArrayList<FileNode>();
+		StringBuilder builder = new StringBuilder();
+		for (FileNode child:filterByClassification(node, "Secret")) {
+				builder.append(child.toString()+"\n");
+		}
+		return builder.toString().trim();
+	}
+
+	public static String filterSecretOrTopSecretFiles(Node node) {
+		filteredNodes = new ArrayList<FileNode>();
+		StringBuilder builder = new StringBuilder();
+		builder.append(filterSecretFiles(node));
+		builder.append("\n");
+		builder.append(filterTopSecretFiles(node));
+		return builder.toString().trim();
+	}
+
+	public static Double getPublicFilesSize(FileNode node) {
+		
+		List<FileNode> result = filterByClassification(node, "Public");
+		Double sum = null;
+		for (Node child:result) {
+			sum += node.getSize();
+		}
+		return sum;
+	}
+
+	private static List<FileNode> filterByClassification(Node node, String classification) {
+
+		if (node instanceof FileNode file && file.getClassification().equalsIgnoreCase(classification)) {
+			filteredNodes.add((FileNode)node);
+		}
+		if (node instanceof DirectoryNode dir && dir.getChildren().size() > 0) {
+
+			for (Node child : ((DirectoryNode) node).getChildren()) {
+				filterByClassification(child, classification);
+			}
+		}
+		return filteredNodes;
+	}
+}
